624d0e7766e9c5a8f9bf958e797ff71a6e282606
ggs5427 <ggs5427@rit.edu>
2020-02-08 16:02:59 -0500

    washing machine

--- server/server/main.py
+++ server/server/main.py
@@ -15,7 +15,6 @@ def hello_world():
     <h2>No washing Machines are available at this time.</h2>
     <p>The next available washing machine will finish it's cycle in: 06:23 minutes</p>
     <img src="/static/washing machine.png" alt="Washing Machine"/>
-    <h1>hello</h1>
     
     """
 
