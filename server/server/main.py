c44a984d6d1bc32671ade506c3851944ff0e4375
Collin Bolles <collinbolles@gmail.com>
2020-02-08 16:26:20 -0500

    Working endpoint for washing machine

--- server/server/main.py
+++ server/server/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, Response
+
 app = Flask(__name__)
 
 
@@ -19,9 +20,28 @@ def hello_world():
     """
 
 
-@app.route('/washer/new_wash_cycle')
-def handle_new_wash_cycle():
-    pass
+@app.route('/washer/user_interaction', methods=['POST'])
+def handle_washer_interaction():
+    """
+    Handles when a user interacts with a washing machine. Updates the status of the washing machine
+    and the number of available washers. Expects information passed in like so.
+    {
+        "washer_id": int,
+        "user_id": int,
+        "washer_status": int,
+        "time_remaining": long (time in seconds, -1 if not applicable)
+    }
+    """
+    data = request.get_json()
+    print(data['washer_id'])
+    washer_id = data['washer_id']
+    user_id = data['user_id']
+    washer_status = data['washer_status']
+    time_remaining = data['time_remaining']
+    print('Washer: {}, By, {}, In state: {}, With {} remaining', washer_id, user_id, washer_status,
+          time_remaining)
+    return Response(status=200)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
