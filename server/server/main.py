9ada31cab4d196947fda4a9623d54e4491c492a0
Saakshi <sd3464@rit.edu>
2020-02-08 16:38:18 -0500

    the flask thingy for helloworld was /hello i changed it to /

--- server/server/main.py
+++ server/server/main.py
@@ -3,20 +3,29 @@ from flask import Flask, request, Response
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():
     return """
     <!DOCTYPE html>
+<html lang="en">
 <head>
-   <title>RIT Laundry Services</title>
+    <title>RIT Laundring Services</title>
 </head>
 <body style="background-color:#ffd1b3;">
-<body>  
-    <h1>Welcome to RIT's Laundry Services</h1>
-    <h2>No washing Machines are available at this time.</h2>
-    <p>The next available washing machine will finish it's cycle in: 06:23 minutes</p>
-    <img src="/static/washing machine.png" alt="Washing Machine"/>
-    
+    <h1>Washing Machines </h1>
+    <img src="/static/OpenWM.png" alt="Washing Machine"/>
+    <form action="/action_page.php" target="_blank" method="GET">
+        <fieldset>
+            <legend>Available washing machines are:</legend>
+            <input type="radio" name="number" value="machine1" > Washing Machine 1<br>
+            <input type="radio" name="number" value="machine2" > Washing Machine 2<br>
+            <input type="radio" name="number" value="machine3" > Washing Machine 3<br>
+            <input type="radio" name="number" value="machine4" > Washing Machine 4<br>
+            <input type="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+</html>
     """
 
 
