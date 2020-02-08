6fc7b15f277b7c267ee187112a37ea5aa749d3d5
ggs5427 <ggs5427@rit.edu>
2020-02-08 15:40:58 -0500

    washing machine

--- server/server/main.py
+++ server/server/main.py
@@ -4,7 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return """
+    <head>
+        <title>RIT Laundring Services</title>
+    </head>
+    <body style="background-color:#ffd1b3;">  
+        <h1>Available Washing Machines are: </h1>
+
+        <form action="/action_page.php" target="_blank" method="GET">
+            <fieldset>
+                <legend>Here:</legend>
+                <input type="radio" name="number" value="machine1" > Washing Machine 1<br>
+                <input type="radio" name="number" value="machine2" > Washing Machine 2<br>
+                <input type="radio" name="number" value="machine3" > Washing Machine 3<br>
+                <input type="radio" name="number" value="machine4" > Washing Machine 4<br>
+                <input type="submit" value="Submit">
+            </fieldset>
+        </form>
+    </body>
+    """
 
 
 if __name__ == '__main__':
