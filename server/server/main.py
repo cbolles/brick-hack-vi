7a6a0c65902f3d5b16ae47557f2089a2ccf7ce7f
Collin Bolles <collinbolles@gmail.com>
2020-02-08 15:56:49 -0500

    Creating controller to handle new wash cycle

--- server/server/main.py
+++ server/server/main.py
@@ -2,10 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/washer/new_wash_cycle')
+def handle_new_wash_cycle():
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
