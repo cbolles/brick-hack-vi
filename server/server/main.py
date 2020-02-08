ac3606552fed4021212c23f82a1009ba4951f872
Saakshi <sd3464@rit.edu>
2020-02-08 15:55:30 -0500

    made the html for no washing machines available

--- server/server/main.py
+++ server/server/main.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return """
+    <!DOCTYPE html>
+<head>
+   <title>RIT Laundry Services</title>
+</head>
+<body style="background-color:#ffd1b3;">
+<body>  
+    <h1>Welcome to RIT's Laundry Services</h1>
+    <h2>No washing Machines are available at this time.</h2>
+    <p>The next available washing machine will finish it's cycle in: 06:23 minutes</p>
+    <img src="/static/washing machine.png" alt="Washing Machine"/>
+    
+    """
 
 
 if __name__ == '__main__':
